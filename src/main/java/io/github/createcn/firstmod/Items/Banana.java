21a209dbbc114928d17255411a2d9d558d23ea27
CreateCN <120872273+CreateCN@users.noreply.github.com>
2024-07-05 21:20:46 +0800

    问题1

--- src/main/java/io/github/createcn/firstmod/Items/Banana.java
+++ src/main/java/io/github/createcn/firstmod/Items/Banana.java
@@ -1,10 +1,17 @@
 package io.github.createcn.firstmod.Items;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemGroup;
-public class Banana extends Item{
+import net.minecraft.world.item.ItemStack;
 
-    public Banana() {
-        super(new Properties().group(ItemGroup.MATERIALS));
+public class Banana extends Item {
+
+    public Banana(){
+        super(new Properties().tab(CreativeModeTab.TAB_MISC));
+    }
+
+    @Override
+    public String getDescriptionId(ItemStack stack) {
+        return "item.io.github.createcn.firstmod.banana";
     }
 }
--- src/main/java/io/github/createcn/firstmod/Items/ItemRegistry.java
+++ src/main/java/io/github/createcn/firstmod/Items/ItemRegistry.java
@@ -1,15 +1,52 @@
 package io.github.createcn.firstmod.Items;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "firstmod");
+    // 使用常量代替硬编码字符串提高可维护性
+    public static final String MOD_ID = "io.github.createcn.firstmod";
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static RegistryObject<Item> Banana = ITEMS.register("banana",)
-    ;{
-        return new Banana();
-    })
+    public static final RegistryObject<Item> BANANA = ITEMS.register("Banana", Banana::new);
+
+    public ItemRegistry() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(modEventBus);
+
+        // 添加事件监听器
+        modEventBus.addListener(this::addCreative);
+    }
+
+    /**
+     * 在创意模式的物品栏中添加香蕉物品。
+     * 目标是将其添加到Miscellaneous物品组中。
+     *
+     * @param event 创意模式物品栏构建内容事件。
+     */
+    private void addCreative(Event event) {
+        if (event instanceof CreativeModeTabEvent.BuildContents) {
+            CreativeModeTabEvent.BuildContents buildContentsEvent = (CreativeModeTabEvent.BuildContents) event;
+            CreativeModeTab tab = buildContentsEvent.getTab();
+        }
+    }
+}
+
+// 假设这个事件类在其他地方定义
+class CreativeModeTabEvent extends Event {
+    static class BuildContents extends CreativeModeTabEvent {
+        private CreativeModeTab tab;
+
+        public CreativeModeTab getTab() {
+            return tab; // 模拟获取当前Tab
+        }
+
+        // 其他必要的方法
+    }
 }
